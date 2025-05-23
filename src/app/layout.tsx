7377671245288b852acb46742b3d40e3ac70e946
Ifeanyichukwu Sampson Ebenezer <40169444+ifeanyidotdev@users.noreply.github.com>
2025-05-23 11:10:17 +0100

    FIX: update tags and metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,26 +1,31 @@
-
-import type {Metadata} from 'next';
-import { Geist_Mono } from 'next/font/google';
-import './globals.css';
+import type { Metadata } from "next";
+import { Geist_Mono } from "next/font/google";
+import "./globals.css";
 import { Toaster } from "@/components/ui/toaster";
 
 const geistMono = Geist_Mono({
-  variable: '--font-geist-mono',
-  subsets: ['latin'],
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
 });
 
 export const metadata: Metadata = {
-  title: 'DevTerm Portfolio',
-  description: 'A developer portfolio with a terminal-like interface.',
+  title: "Ifeanyi's Portfolio",
+  description: "A developer portfolio with a terminal-like interface.",
+  icons: {
+    icon: "./favicon.ico",
+    origin: "./favicon.ico",
+    apple: "./favicon.ico",
+  },
 };
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode; 
+  children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="dark" suppressHydrationWarning={true}>{/* Default to dark theme, can be changed by 'theme' command */}
+    <html lang="en" className="dark" suppressHydrationWarning={true}>
+      {/* Default to dark theme, can be changed by 'theme' command */}
       <body className={`${geistMono.variable} font-mono antialiased h-full`}>
         {children}
         <Toaster />
